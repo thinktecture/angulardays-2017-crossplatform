8e6d759fe80ce91665b96b9e05d305a7c66da47e
Andreas Wissel <kaeku314@gmail.com>
2017-10-26 21:24:35 +0200

    fixes wonky animations

--- src/app/helpers/route-animation.ts
+++ src/app/helpers/route-animation.ts
@@ -2,28 +2,28 @@ import {animate, group, query, style, transition, trigger} from '@angular/animat
 
 export const routeTransition = trigger('routeTransition', [
   transition('starwars => home, pokemon => home, mirror => home', [
-    query(':enter, :leave', style({position: 'fixed', width: '100%'}), {optional: true}),
+    query(':enter, :leave', style({position: 'fixed'}), {optional: true}),
     group([
       query(':enter', [
-        style({transform: 'translateX(-100%)'}),
-        animate('.5s ease-in-out', style({transform: 'translateX(0)'}))
+        style({transform: 'translate3d(-100%, 0, 0)', opacity: 0}),
+        animate('.5s ease-in-out', style({transform: 'translate3d(0, 0, 0)', opacity: 1}))
       ], {optional: true}),
       query(':leave', [
-        style({transform: 'translateX(0)'}),
-        animate('.5s ease-in-out', style({transform: 'translateX(100%)'}))
+        style({transform: 'translate3d(0, 0, 0)'}),
+        animate('.5s ease-in-out', style({transform: 'translate3d(100%, 0, 0)'}))
       ], {optional: true})
     ])
   ]),
   transition('* <=> *', [
-    query(':enter, :leave', style({position: 'fixed', width: '100%'}), {optional: true}),
+    query(':enter, :leave', style({position: 'fixed'}), {optional: true}),
     group([
       query(':enter', [
-        style({transform: 'translateX(100%)'}),
-        animate('.5s ease-in-out', style({transform: 'translateX(0)'}))
+        style({transform: 'translate3d(100%, 0, 0)', opacity: 0}),
+        animate('.5s ease-in-out', style({transform: 'translate3d(0, 0, 0)', opacity: 1}))
       ], {optional: true}),
       query(':leave', [
-        style({transform: 'translateX(0)'}),
-        animate('.5s ease-in-out', style({transform: 'translateX(-100%)'}))
+        style({transform: 'translate3d(0, 0, 0)'}),
+        animate('.5s ease-in-out', style({transform: 'translate3d(-100%, 0, 0)'}))
       ], {optional: true})
     ])
   ]),
