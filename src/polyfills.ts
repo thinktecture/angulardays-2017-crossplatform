67ae98b831d8db172f3cebc36f120a4fac46b49e
Andreas Wissel <kaeku314@gmail.com>
2017-10-26 21:42:49 +0200

    add web animation polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -33,7 +33,7 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js';
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
