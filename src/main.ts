b43b852d66e8311f29075210479e64ad62e6c763
Andreas Wissel <kaeku314@gmail.com>
2017-10-27 12:41:59 +0200

    fixes scrollbar positioning

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {enableProdMode} from "@angular/core";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 
-import {environment} from './environments/environment';
-import {hmrBootstrap} from './hmr';
-import {AppModule} from './app/module';
+import {environment} from "./environments/environment";
+import {hmrBootstrap} from "./hmr";
+import {AppModule} from "./app/module";
 
 if (environment.production) {
   enableProdMode();
